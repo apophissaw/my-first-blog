517a36a33e36d7065e8f5e8637504e6866feda75
gabi <apophissaw@gmail.com>
2015-08-10 00:09:04 +0200

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,40 @@
+# if ( 3 > 2):
+#     print('five is greater than 2')
+# else:
+#     print('five is not greater than 2')
+#
+# name = 'Ola'
+# if name == 'Ola':
+#     print('Hey Ola')
+# elif name == 'Sonja':
+#     print('Hey Sonja')
+# else:
+#     print('Hey Anonymus')
+#
+# def hi():
+#     print('Hi there!')
+#     print('How are you!')
+# hi()
+#
+# def hi(name):
+#     if name == 'gabi':
+#         print('hello, its me')
+#     else:
+#         print('sadly, it is not me at this time')
+#
+# hi('lola')
+
+# def hi(name):
+    # print('hi' + name '!')
+
+
+
+
+def hi(name):
+	print('Hi ' + name  + '!')
+	
+girls = ['Rachel' , 'Monica', 'Phoebe','Ola', 'You']	
+
+for name in girls:
+	hi(name)
+	print('Next girl')
